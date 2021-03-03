050beca37e1900c67da0011b60c24de4e2f3a69d
Jess Sullivan <jess.sullivan@wearewildgoose.com>
2021-03-03 15:14:23 +0000

    init

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Person.destroy_all
+
+Person.create! [
+  { first_name: "Kalman", last_name: "Smith", age: 33 },
+  { first_name: "John", last_name: "Whatever", age: 27 },
+  { first_name: "Michael", last_name: "Smith", age: 15 },
+  { first_name: "Josh", last_name: "Oreck", age: 57 },
+  { first_name: "John", last_name: "Smith", age: 27 },
+  { first_name: "Bill", last_name: "Gates", age: 75 },
+  { first_name: "Lebron", last_name: "James", age: 30 }
+]
